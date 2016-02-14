a6cf04c34aab3d964e118b72867a5852b0b44f57
Enache Catalin <enachecatalin@Enaches-MacBook-Pro.local>
2016-02-14 14:43:30 +0200

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20160212151740) do
 
-  create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",      default: "", null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
