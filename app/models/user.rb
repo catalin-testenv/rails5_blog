1c57eed8616ee7ed92bc8eb3bd6bebd40c5e10ce
Catalin Enache <catalinenache78s@gmail.com>
2016-02-20 16:49:14 +0200

    make first user admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:google_oauth2]
   enum role: [ :commenter, :admin ]
 
+  before_create do |user|
+    user.role = :admin if User.unscoped.count == 0
+  end
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
