2cc578f41857d91ac525347b3d93e961480b9c3e
Catalin Enache <catalinenache78s@gmail.com>
2016-02-20 10:50:56 +0200

    refactor assets loading

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,19 +4,14 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-ENV['ASSETS_PATHS'].split(':').each do |path|
-  Rails.application.config.assets.paths << File.join(Rails.root, path)
-end
+# Rails.application.config.assets.paths << File.join(Rails.root, '/path')
 
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( **/*.jpg )
 
-# Extend Sass.load_paths from ENV
-ENV['SASS_LOAD_PATHS'].split(':').each do |path|
-  Sass.load_paths << File.join(Rails.root, path)
-end
+# Extend Sass.load_paths
+# Sass.load_paths << File.join(Rails.root, '/path')
 
-p Sass.load_paths
 
