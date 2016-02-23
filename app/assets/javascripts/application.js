16a8cfefa5370a83d17e936c87ee68337e10c312
Catalin Enache <catalinenache78s@gmail.com>
2016-02-23 23:16:40 +0200

    add support for ace code editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,12 +12,57 @@
 //
 //= require jquery
 //= require jquery_ujs
+
 // https://github.com/zurb/foundation-sites | bower install foundation-sites
 //= require foundation
+
 // http://stickyjs.com/ | https://github.com/garand/sticky
 //= require jquery.sticky
-// http://www.gerd-tentler.de/tools/codeview/
-//= require codeview
+
+// ============== ace editor
+// https://ace.c9.io/#nav=howto
+// https://github.com/ajaxorg/ace-builds
+//= require ace
+//= require mode-css
+//= require mode-html_ruby
+//= require mode-html
+//= require mode-javascript
+//= require theme-monokai
+//= require ext-language_tools
+// ============== /ace editor
+
 //= require cable
 
 $(function(){ $(document).foundation(); });
+
+// ========= ace editors
+
+$(document).ready(function(){
+    ace.config.setDefaultValue("session", "useWorker", false);
+    //ace.require("ace/ext/language_tools");
+
+    var ace_editor_js = ace.edit("ace_editor_js");
+    ace_editor_js.setTheme("ace/theme/monokai");
+    ace_editor_js.getSession().setMode("ace/mode/javascript");
+    //ace_editor_js.setReadOnly(true);
+    //ace_editor_js.resize();
+    ace_editor_js.setOptions({
+        enableBasicAutocompletion: true,
+        //enableSnippets: true,
+        enableLiveAutocompletion: false
+    });
+    ace_editor_js.$blockScrolling = Infinity; // to disable a deprecation warning
+
+    var ace_editor_css = ace.edit("ace_editor_css");
+    ace_editor_css.setTheme("ace/theme/monokai");
+    ace_editor_css.getSession().setMode("ace/mode/css");
+    //ace_editor_css.setReadOnly(true);
+    //ace_editor_css.resize();
+    ace_editor_css.setOptions({
+        enableBasicAutocompletion: true,
+        //enableSnippets: true,
+        enableLiveAutocompletion: false
+    });
+    ace_editor_css.$blockScrolling = Infinity; // to disable a deprecation warning
+});
+
