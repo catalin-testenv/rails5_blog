6cef7290db56fb92f555bbcc49b6c971f3d5a93d
Catalin Enache <catalinenache78s@gmail.com>
2016-03-07 14:43:07 +0200

    allow quick ruby code in tinymce

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,7 @@ module ApplicationHelper
   end
 
   def evaluate(data)
+    # allow quick `[ruby_code_here]`
     data = data.gsub(/(`\[|\]`)/, '`[' => '<%= ', ']`' => ' %>')
     ERB.new(data).result(binding)
   end
