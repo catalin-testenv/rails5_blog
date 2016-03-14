a88f1bfdcb5568f1b4a4923c26711af8019b5b71
Catalin Enache <catalinenache78s@gmail.com>
2016-03-14 13:51:17 +0200

    updates

---
+++ app/helpers/miscellaneous_helper.rb
@@ -0,0 +1,28 @@
+module MiscellaneousHelper
+
+  def admin_body_title(title)
+    "<h4 class='uk-panel-title'>#{ title }</h4>".html_safe
+  end
+
+  def admin_body_top_links(links)
+    html = ''
+    html << '<div><ul class="uk-subnav uk-subnav-line uk-link-unmuted">'
+    html << links.map do |ln|
+      %Q`<li>#{ link_to(ln[:message], ln[:path], ln[:options]) }</li>`
+    end.join("\n")
+    html << '</ul></div>'
+    html << '<hr class="uk-grid-divider uk-margin-top-remove cancel-panel-box-horizontal-padding" />'
+    html.html_safe
+  end
+
+  def admin_body_bottom_links(links)
+    html = ''
+    html << '<hr class="uk-grid-divider cancel-panel-box-horizontal-padding" />'
+    html << '<div><ul class="uk-subnav uk-subnav-line uk-margin-bottom-remove uk-link-unmuted">'
+    html << links.map do |ln|
+      %Q`<li class="uk-margin-top-remove">#{ link_to(ln[:message], ln[:path], ln[:options]) }</li>`
+    end.join("\n")
+    html << '</ul></div>'
+    html.html_safe
+  end
+end
