b7eb0465026fbfee356dd3454a8e381afaa2482d
Catalin Enache <catalinenache78s@gmail.com>
2016-03-15 23:32:45 +0200

    updates

--- config/initializers/list_ops_config.rb
+++ config/initializers/list_ops_config.rb
@@ -1,6 +1,5 @@
 
 ActionController::Parameters.always_permitted_parameters += %w(orderby direction)
-# ActionController::Parameters.always_permitted_parameters += [:current_page, :page, :total_pages, :per_page, :remote, :paginator]
 
 class ActionController::Base
   include ListOps
