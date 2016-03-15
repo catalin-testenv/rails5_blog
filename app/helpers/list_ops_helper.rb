84ea83fdfe01010e736bbed76ce2da4b9a21b7cd
Catalin Enache <catalinenache78s@gmail.com>
2016-03-16 00:12:49 +0200

    updates

--- app/helpers/list_ops_helper.rb
+++ app/helpers/list_ops_helper.rb
@@ -1,11 +1,26 @@
 module ListOpsHelper
 
-  def sort_link(orderby, title = nil)
+  def list_ops_sort_link(orderby, title = nil)
     orderby = orderby.to_s
     title ||= orderby.titleize
     direction = (orderby == list_ops_sort_orderby && list_ops_sort_direction == 'asc') ? 'desc' : 'asc'
     css_class = orderby == list_ops_sort_orderby ? "sort_link_current #{list_ops_sort_direction}" : nil
     link_to title, request.query_parameters.merge({orderby: orderby, direction: direction}), {class: css_class}
   end
+
+  def list_ops_filter(column, model, key)
+    col = model.columns.find do |col|
+      column.to_s == col.name
+    end
+
+    case col.type
+      when :string
+        %Q`<input type="text" name="#{key}" />`.html_safe
+      when :integer
+        'integer'
+    end
+  end
+
+
 
 end
