bc02e40aaba9cb4881533504202be2fc1f3945f2
Catalin Enache <catalinenache78s@gmail.com>
2016-03-16 10:46:15 +0200

    updates

--- app/controllers/concerns/list_ops.rb
+++ app/controllers/concerns/list_ops.rb
@@ -1,12 +1,14 @@
 
 # related files: list_ops list_ops_helper list_ops_config
 
-# include this concern in ApplicationController
+# list_ops_config initializer auto-includes this module in ActionController::Base
 module ListOps
   extend ActiveSupport::Concern
 
   included do
 
+    # To be used in controller's index like methods.
+    # ex: @resource_list = list_ops_sort(Model.all)
     def list_ops_sort(scope)
       if list_ops_sort_orderby != ''
         scope.reorder("#{list_ops_sort_orderby} #{list_ops_sort_direction}")
@@ -15,9 +17,9 @@ module ListOps
       end
     end
 
-    # this method is trying to infer model name from controller name
-    # target controller might override this method when autodetect does not do the job
-    # ex. return:  Model.column_names
+    # This method is trying to infer model name from controller name.
+    # Target controller can override this when autodetect does not do the job.
+    # Override method would return:  Model.column_names
     def list_ops_sortable_columns
       self.class.name.split('::')[-1].sub('Controller', '').singularize.constantize.column_names
     end
