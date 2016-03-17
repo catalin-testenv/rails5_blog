3368f55f01d4071a64b2970839b218afdc4d6257
Catalin Enache <catalinenache78s@gmail.com>
2016-03-17 11:04:58 +0200

    updates

--- app/helpers/list_ops_helper.rb
+++ app/helpers/list_ops_helper.rb
@@ -6,35 +6,35 @@ module ListOpsHelper
     title ||= orderby.titleize
     direction = (orderby == list_ops_sort_orderby && list_ops_sort_direction == 'asc') ? 'desc' : 'asc'
     css_class = orderby == list_ops_sort_orderby ? "sort_link_current #{list_ops_sort_direction}" : nil
-    link_to title, qp.merge({orderby: orderby, direction: direction}), {class: css_class}
+    link_to title, qp.merge({orderby: orderby, direction: direction}), {class: [css_class]}
   end
 
   def list_ops_filter_string(model, column, flt_key)
     qp = request.query_parameters
     content_tag :div, class: %w(uk-form-row) do
-      concat label_tag(flt_key, model.human_attribute_name(column).humanize, class: 'uk-form-label')
+      concat label_tag(flt_key, model.human_attribute_name(column).humanize, class: %w(uk-form-label))
       concat(content_tag(:div, class: %w(uk-form-controls)) do
-        text_field_tag(flt_key, qp[flt_key], class: 'uk-width')
+        text_field_tag(flt_key, qp[flt_key], class: %w(uk-width))
       end)
     end
   end
 
   def list_ops_filter_boolean(model, column, flt_key, yes, no, wildcard)
     qp = request.query_parameters
-    content_tag :div, class: ['uk-form-row'] do
-      concat label_tag(nil, model.human_attribute_name(column).humanize, class: 'uk-form-label')
+    content_tag :div, class: %w(uk-form-row) do
+      concat label_tag(nil, model.human_attribute_name(column).humanize, class: %w(uk-form-label))
       concat(content_tag(:div, class: %w(uk-grid uk-grid-collapse)) do
         concat(content_tag(:div, class: %w(uk-width-1-3)) do
           concat radio_button_tag(flt_key, '', ['', nil].include?(qp[flt_key]))
-          concat label_tag("#{flt_key}_", wildcard, class: 'uk-width')
+          concat label_tag("#{flt_key}_", wildcard, class: %w(uk-width))
         end)
         concat(content_tag(:div, class: %w(uk-width-1-3)) do
           concat radio_button_tag(flt_key, '0', qp[flt_key] == '0')
-          concat label_tag("#{flt_key}_0", no, class: 'uk-width')
+          concat label_tag("#{flt_key}_0", no, class: %w(uk-width))
         end)
         concat(content_tag(:div, class: %w(uk-width-1-3)) do
           concat radio_button_tag(flt_key, '1', qp[flt_key] == '1')
-          concat label_tag("#{flt_key}_1", yes, class: 'uk-width')
+          concat label_tag("#{flt_key}_1", yes, class: %w(uk-width))
         end)
       end)
     end
@@ -42,17 +42,17 @@ module ListOpsHelper
 
   def list_ops_filter_datetime_range(model, column, flt_key, start_key, end_key, start_label, end_label)
     qp = request.query_parameters
-    content_tag :div, class: ['uk-form-row'] do
-      concat label_tag(flt_key, model.human_attribute_name(column).humanize, class: 'uk-form-label')
-      concat(content_tag(:div, class: ['uk-form-controls']) do
-        content_tag :div, class: ['uk-grid'] do
-          concat(content_tag(:div, class: ['uk-width-1-2']) do
-            concat label_tag("#{flt_key}[#{start_key}]", start_label.humanize+':', class: 'uk-form-label')
-            concat text_field_tag("#{flt_key}[#{start_key}]", qp[flt_key] && qp[flt_key][start_key], class: 'uk-width', data: {'uk-datepicker' => ''})
+    content_tag :div, class: %w(uk-form-row) do
+      concat label_tag(flt_key, model.human_attribute_name(column).humanize, class: %w(uk-form-label))
+      concat(content_tag(:div, class: %w(uk-form-controls)) do
+        content_tag :div, class: %w(uk-grid) do
+          concat(content_tag(:div, class: %w(uk-width-1-2)) do
+            concat label_tag("#{flt_key}[#{start_key}]", start_label.humanize+':', class:  %w(uk-form-label))
+            concat text_field_tag("#{flt_key}[#{start_key}]", qp[flt_key] && qp[flt_key][start_key], class: %w(uk-width), data: {'uk-datepicker' => ''})
           end)
-          concat(content_tag(:div, class: ['uk-width-1-2']) do
-            concat label_tag("#{flt_key}[#{end_key}]", end_label.humanize+':', class: 'uk-form-label')
-            concat text_field_tag("#{flt_key}[#{end_key}]", qp[flt_key] && qp[flt_key][end_key], class: 'uk-width')
+          concat(content_tag(:div, class: %w(uk-width-1-2)) do
+            concat label_tag("#{flt_key}[#{end_key}]", end_label.humanize+':', class:  %w(uk-form-label))
+            concat text_field_tag("#{flt_key}[#{end_key}]", qp[flt_key] && qp[flt_key][end_key], class: %w(uk-width))
           end)
         end
       end)
