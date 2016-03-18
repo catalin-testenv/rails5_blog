25094d4fb61fccbd8045c3fe6798036e04ba7db4
Catalin Enache <catalinenache78s@gmail.com>
2016-03-18 22:41:56 +0200

    updates

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,56 +1,54 @@
 module FormHelper
 
   def form_global_errors(resource, model)
-    html = ''
     if resource.errors.any?
-      html << %Q`
-      <div class="uk-alert uk-alert-danger cancel-panel-box-horizontal-padding">
-        <h4>#{resource_view_form_general_error_message resource: model, errors_num: resource.errors_num }:</h4>
-        <ul>`
-         resource.errors.full_messages.each do |message|
-            html << "<li>#{message}</li>"
-         end
-      html << %q`</ul>
-      </div>
-      `
+      content_tag :div, class: %w(uk-alert uk-alert-danger cancel-panel-box-horizontal-padding) do
+        concat(content_tag(:h4) do
+          resource_view_form_general_error_message resource: model, errors_num: resource.errors_num
+        end)
+        concat(content_tag(:ul) do
+          resource.errors.full_messages.map do |message|
+            content_tag :li do
+              message
+            end
+          end.join("\n").html_safe
+        end)
+      end
     end
-    html.html_safe
   end
 
   def form_input(f, type, name)
-    html = ''
-    html << f.label(name, class: 'uk-form-label') do |e|
-      f.object.class.human_attribute_name(name)
+    capture do
+      concat(label_tag(name, class: %w(uk-form-label)) do
+        f.object.class.human_attribute_name(name)
+      end)
+      concat(content_tag(:div, class: %w(uk-form-controls)) do
+        f.send(type, name, class: 'uk-width')
+      end)
+      concat(f.object.errors[name].map do |e|
+        content_tag(:p, class: %w(uk-form-help-block uk-text-danger)) do
+          "#{f.object.class.human_attribute_name(name) } #{e}"
+        end
+      end.join("\n").html_safe)
     end
-    html << %Q`
-    <div class="uk-form-controls">
-      #{f.send(type, name, class: 'uk-width')}
-    </div>
-    `
-    html << f.object.errors[name].map do |e|
-        %Q`<p class="uk-form-help-block uk-text-danger">#{f.object.class.human_attribute_name(name) } #{e}</p>`
-    end.join("\n")
-    html.html_safe
   end
 
   def form_select(f, name, options, selected, select_options={})
-    html = ''
-    html << f.label(name, class: 'uk-form-label') do |e|
-      f.object.class.human_attribute_name(name)
+    capture do
+      concat(label_tag(name, class: %w(uk-form-label)) do
+        f.object.class.human_attribute_name(name)
+      end)
+      concat(content_tag(:div, class: %w(uk-form-controls)) do
+        f.select(name, options_for_select(options, :selected => selected), select_options, {class: 'uk-width'})
+      end)
     end
-    html << %Q`
-    <div class="uk-form-controls">
-      #{f.select(name, options_for_select(options, :selected => selected), select_options, {class: 'uk-width'})}
-    </div>
-    `
-    html.html_safe
   end
 
   def form_check_box(f, name)
-    html = ''
-    html << f.check_box(name)
-    html << f.label(name, f.object.class.human_attribute_name(name), class: 'uk-width')
-    html.html_safe
+    capture do
+      concat f.check_box(name)
+      concat f.label(name, f.object.class.human_attribute_name(name), class: 'uk-width')
+    end
   end
 
 end
--- app/helpers/list_ops_helper.rb
+++ app/helpers/list_ops_helper.rb
@@ -9,7 +9,7 @@ module ListOpsHelper
     link_to title, qp.merge({orderby: orderby, direction: direction}), {class: [css_class]}
   end
 
-  def list_ops_filter_string(model, column, flt_key)
+  def list_ops_string(model, column, flt_key)
     qp = request.query_parameters
     content_tag :div, class: %w(uk-form-row) do
       concat label_tag(flt_key, model.human_attribute_name(column).humanize, class: %w(uk-form-label))
@@ -19,7 +19,7 @@ module ListOpsHelper
     end
   end
 
-  def list_ops_filter_boolean(model, column, flt_key, yes, no, wildcard)
+  def list_ops_boolean(model, column, flt_key, yes, no, wildcard)
     qp = request.query_parameters
     content_tag :div, class: %w(uk-form-row) do
       concat label_tag(nil, model.human_attribute_name(column).humanize, class: %w(uk-form-label))
@@ -40,18 +40,16 @@ module ListOpsHelper
     end
   end
 
-  def list_ops_filter_datetime_range(model, column, flt_key, start_key, end_key, start_label, end_label)
+  def list_ops_datetime_range(model, column, flt_key, start_key, end_key, start_label, end_label)
     qp = request.query_parameters
     content_tag :div, class: %w(uk-form-row) do
       concat label_tag(flt_key, model.human_attribute_name(column).humanize, class: %w(uk-form-label))
       concat(content_tag(:div, class: %w(uk-form-controls)) do
         content_tag :div, class: %w(uk-grid uk-grid-small) do
           concat(content_tag(:div, class: %w(uk-width-small-1-2)) do
-            # concat label_tag("#{flt_key}[#{start_key}]", start_label.humanize+':', class:  %w(uk-form-label))
             concat text_field_tag("#{flt_key}[#{start_key}]", qp[flt_key] && qp[flt_key][start_key], class: %w(uk-width), data: {'uk-datepicker' => ''}, placeholder: start_label.humanize)
           end)
           concat(content_tag(:div, class: %w(uk-width-small-1-2)) do
-            # concat label_tag("#{flt_key}[#{end_key}]", end_label.humanize+':', class:  %w(uk-form-label))
             concat text_field_tag("#{flt_key}[#{end_key}]", qp[flt_key] && qp[flt_key][end_key], class: %w(uk-width), data: {'uk-datepicker' => ''}, placeholder: end_label.humanize)
           end)
         end
@@ -59,7 +57,7 @@ module ListOpsHelper
     end
   end
 
-  def list_ops_filter_select(model, column, flt_key, options, html_options={class: 'uk-width'})
+  def list_ops_select(model, column, flt_key, options, html_options={class: 'uk-width'})
     qp = request.query_parameters
     content_tag :div, class: %w(uk-form-row) do
       concat label_tag(flt_key, model.human_attribute_name(column).humanize, class: %w(uk-form-label))
