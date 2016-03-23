c31e612fa608e02f8fc3f6149621e355c9e94683
Catalin Enache <catalinenache78s@gmail.com>
2016-03-23 13:31:59 +0200

    updates

--- app/helpers/layout_regions_helper.rb
+++ app/helpers/layout_regions_helper.rb
@@ -3,10 +3,5 @@ module LayoutRegionsHelper
   def get_content_for(region)
     LayoutRegion.find_by(name: region).try(:html_safe)
   end
-
-  # TODO: fake
-  def some_helper
-    'helper content'
-  end
-
+  
 end
