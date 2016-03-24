7dea250e07ae9bc1fac2cdcaa411ae9d601b027e
Catalin Enache <catalinenache78s@gmail.com>
2016-03-24 22:56:34 +0200

    move children when page_category is destroyed

--- app/models/page_category.rb
+++ app/models/page_category.rb
@@ -6,12 +6,19 @@ class PageCategory < ApplicationRecord
   has_many :subcategories, foreign_key: :parent_id, class_name: 'PageCategory'
   belongs_to :parent, class_name: 'PageCategory', required: false
 
+  before_destroy :move_children
+
   validates :name, presence: true, uniqueness: true
 
   default_scope { order(:parent_id, :ordering) }
 
   def children
     (pages + subcategories).sort_by { |item| [item.parent_id.to_i, item.ordering.to_i] }
+  end
+
+  def move_children
+    pages.update_all(parent_id: parent.try(:id) || 0)
+    subcategories.update_all(parent_id: parent.try(:id) || 0)
   end
 
   def has_subcategories?
