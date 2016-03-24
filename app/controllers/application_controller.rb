0e5ea510196a8a7f3d6cfaa84ef4d03052141b75
Catalin Enache <catalinenache78s@gmail.com>
2016-03-24 22:58:24 +0200

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 
 # TODO: make page and page_category unique per scope
-# TODO: what happens when a page category is destroyed ?
 # TODO: find a way to dynamically load js/css per admin/site and theme |eventually put templates in DB ?
 # TODO: find a way to not override a user theme when main blog code is updated from repo
 # TODO: should be excluded from PageCategory show listing those pages for main nav?
