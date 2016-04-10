c800475944767084a9c732667e2c59cae3aeb07e
Catalin Enache <catalinenache78s@gmail.com>
2016-04-10 16:03:09 +0300

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,12 @@ content = <<'END'
     ['hello', 'world'].map{|w|"<li>#{w}</li>"}.join("\n").html_safe
   %>
 </ul>
+<pre data-editor-ace='{"sessionOptions": {"language":"javascript"}, "editorOptions":{"theme":"ace/theme/cobalt"}}'>
+
+    function myFunc () {
+        return 'hello';
+    }
+</pre>
 <h2>The standard Lorem Ipsum passage, used since the 1500s</h2>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
